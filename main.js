aac8e599899f21364d319f7938eee137cabbe60e
Neil <neil@logan-5.co.uk>
2025-09-16 18:25:09 +0100

    Update policy links to use local files and prepare for domain migration
    - Change policy URLs from external warpfactor.co.uk to local ./policies/ files
    - Add local privacy_policy.html and terms_of_service.html in policies folder
    - Update service worker cache version
    - Self-contained policies work on any domain automatically
    - Ready for migration to www.korovatron.co.uk

--- main.js
+++ main.js
@@ -241,13 +241,13 @@ function setupCanvasEventListeners() {
                 
                 // Privacy policy link
                 if (isClickOnPrivacyPolicyLink(mouseX, mouseY)) {
-                    window.open('https://warpfactor.co.uk/crate_escape_policies/privacy_policy.html', '_blank');
+                    window.open('./policies/privacy_policy.html', '_blank');
                     return;
                 }
                 
                 // Terms of service link
                 if (isClickOnTermsOfServiceLink(mouseX, mouseY)) {
-                    window.open('https://warpfactor.co.uk/crate_escape_policies/terms_of_service.html', '_blank');
+                    window.open('./policies/terms_of_service.html', '_blank');
                     return;
                 }
             }
@@ -485,13 +485,13 @@ function setupCanvasEventListeners() {
                         
                         // Privacy policy link
                         if (isClickOnPrivacyPolicyLink(canvasPos.x, canvasPos.y)) {
-                            window.open('https://warpfactor.co.uk/crate_escape_policies/privacy_policy.html', '_blank');
+                            window.open('./policies/privacy_policy.html', '_blank');
                             return;
                         }
                         
                         // Terms of service link
                         if (isClickOnTermsOfServiceLink(canvasPos.x, canvasPos.y)) {
-                            window.open('https://warpfactor.co.uk/crate_escape_policies/terms_of_service.html', '_blank');
+                            window.open('./policies/terms_of_service.html', '_blank');
                             return;
                         }
                     }
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'crate-escape-v7.4';
+const CACHE_NAME = 'crate-escape-v7.5';
 const urlsToCache = [
   './',
   './index.html',
