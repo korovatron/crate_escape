8f3217dc8399920b2d91903213bfd78f177f6eda
Neil <neil@logan-5.co.uk>
2026-02-26 21:17:06 +0000

    Bump app and cache version to 1.1.2

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // #region Event Handlers & Input
 "use strict";
-const APP_VERSION = '1.1.1';
+const APP_VERSION = '1.1.2';
 const pressedKeys = new Set();
 const lastKeyTime = new Map(); // Track when each key was last processed
 const keyDebounceDelay = 500; // Half second delay for key repeat
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'crate-escape-version-1.1.1';
+const CACHE_NAME = 'crate-escape-version-1.1.2';
 const urlsToCache = [
   './',
   './index.html',
