c5dcc4bbe6b9fd945af286bd529ab99d9cf91b90
Neil <neil@logan-5.co.uk>
2026-03-03 19:47:25 +0000

    v1.1.78

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // #region Event Handlers & Input
 "use strict";
-const APP_VERSION = '1.1.77';
+const APP_VERSION = '1.1.78';
 const pressedKeys = new Set();
 const lastKeyTime = new Map(); // Track when each key was last processed
 const keyDebounceDelay = 500; // Half second delay for key repeat
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'crate-escape-version-1.1.77';
+const CACHE_NAME = 'crate-escape-version-1.1.78';
 const urlsToCache = [
   './',
   './index.html',
